ba0d3892a27bf25fe172fec6097391049be9c664
Geson-anko <59220704+Geson-anko@users.noreply.github.com>
2025-02-04 02:47:02 +0000

    CMakeLists.txt: Change inputtino library link from PRIVATE to PUBLIC; update README with include path for inputtino; add inputtino header in main.cpp

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ NOTE: you must add include paths to `c_cpp_properties.json`
 ```sh
 pybind11-config --includes
 ```
+
+also need
+
+```
+"./build/cp312-cp312-linux_x86_64/_deps/inputtino-src/include"
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 // Copyright 2025 Geson-anko
 #include <pybind11/pybind11.h>
 
+#include <inputtino/input.hpp>
+
 std::string hello_from_bin() { return "Hello from inputtino-python!"; }
 
 namespace py = pybind11;
